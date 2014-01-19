ceb908cc1bb8521c235984bade1bc69659ff85c7
Christian Leutloff <leutloff@sundancer.oche.de>
2014-01-19 17:47:27 +0100

    Change test app to port 80.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 var app = express();
 
 // Configuration
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
--- test/simple_test.js
+++ test/simple_test.js
@@ -13,20 +13,18 @@ process.on('uncaughtException', function(e) {
 
 
 console.log('process.env.TRAVIS: %s', process.env.TRAVIS);
-console.log('(process.env.TRAVIS == \'true\'): %s', (process.env.TRAVIS == 'true'));
 console.log('(process.env.TRAVIS === \'true\'): %s', (process.env.TRAVIS === 'true'));
 
-//console.log('(process.env.TRAVIS == true): %s', (process.env.TRAVIS == true));
-//console.log('(process.env.TRAVIS === true): %s', (process.env.TRAVIS === true));
-//console.log('(process.env.TEST_RUN_LOCAL === false): %s', (process.env.TEST_RUN_LOCAL === false));
-//console.log('(process.env.TEST_RUN_LOCAL || false): %s', (process.env.TEST_RUN_LOCAL || false));
-//console.log('((process.env.TEST_RUN_LOCAL || false) === false): %s', ((process.env.TEST_RUN_LOCAL || false) === false));
-//console.log('process.env.TRAVIS && !(process.env.TEST_RUN_LOCAL || false): %s', (process.env.TRAVIS && !(process.env.TEST_RUN_LOCAL || false)));
-//console.log('process.env.TRAVIS && !(process.env.TEST_RUN_LOCAL || false): %s', ((process.env.TRAVIS === true) && (process.env.TEST_RUN_LOCAL === false)));
+console.log('(process.env.TEST_RUN_LOCAL === false): %s', (process.env.TEST_RUN_LOCAL === 'false'));
+console.log('(process.env.TEST_RUN_LOCAL || false): %s', (process.env.TEST_RUN_LOCAL || false));
+console.log('((process.env.TEST_RUN_LOCAL || false) === false): %s', ((process.env.TEST_RUN_LOCAL || false) !== 'true'));
+console.log('process.env.TRAVIS && !(process.env.TEST_RUN_LOCAL || false): %s', ((process.env.TRAVIS === 'true') && !(process.env.TEST_RUN_LOCAL || false)));
+console.log('process.env.TRAVIS === true && !(process.env.TEST_RUN_LOCAL || false): %s', ((process.env.TRAVIS === 'true') && (process.env.TEST_RUN_LOCAL === 'false')));
 
 //if (process.env.TRAVIS && !(process.env.TEST_RUN_LOCAL || false)) {
-if (process.env.TRAVIS) {
+if ((process.env.TRAVIS === 'true')) {
     console.log('running tests on SauceLabs using sauce connect...');
+    console.log('TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')');
 
     var BROWSERNAME = process.env._BROWSER || process.env.BROWSER || 'chrome';
     var BROWSERVERSION = process.env._VERSION || process.env.VERSION || '*';
@@ -40,7 +38,7 @@ if (process.env.TRAVIS) {
             version: BROWSERVERSION,
             platform: BROWSERPLATFORM,
             tags: ['examples'],
-            name: 'Run single page test using webdriverjs/Selenium.',
+            name: 'Run a \'simple test\' using webdriverjs/Selenium.',
         },
         // for w/o sauce connect
         //      host: 'ondemand.saucelabs.com',
@@ -90,7 +88,8 @@ describe('Run a \'simple test\' using webdriverjs/Selenium.', function() {
     beforeEach(function(done) {
         this.timeout(10000); // some time is needed for the browser start up, on my system 3000 should work, too.
         // Navigate to the URL for each test
-        client.url('http://localhost:3000')
+        //client.url('http://localhost:3000')
+        client.url('http://localhost:80')
         .call(done);
     });
 
