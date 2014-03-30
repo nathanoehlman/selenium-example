037625b681541cdd8265ac331c83426e5081d009
Christian Leutloff <leutloff@sundancer.oche.de>
2014-03-30 23:46:31 +0200

    use a supported version of the selenium version for running on saucelabs.

--- test/simple_internet.js
+++ test/simple_internet.js
@@ -18,7 +18,8 @@ if ((process.env.TRAVIS === 'true') && (process.env.TEST_RUN_LOCAL !== 'true')) 
     var BROWSERPLATFORM = (process.env._PLATFORM || process.env.PLATFORM || 'Linux').replace(/_/g,' ');
     var BUILDID = process.env.TRAVIS_BUILD_ID || 'unknown-buildid';
     var TUNNELIDENTIFIER = process.env.TRAVIS_JOB_NUMBER || 'unknown-jobnumber';
-    var SELENIUMVERSION = '2.41.0';
+    // select selenium version - for available versions see https://saucelabs.com/docs/additional-config#selenium-version
+    var SELENIUMVERSION = '2.40.0';
 
     console.log('BROWSERNAME: ' + BROWSERNAME);
     console.log('BROWSERVERSION: ' + BROWSERVERSION);
