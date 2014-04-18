64651f69a8aa5300b9bda38267e6342f15748c73
Christian Leutloff <leutloff@sundancer.oche.de>
2014-04-18 21:50:04 +0200

    version 1.1.0: updated express to version 4.x including a rework main app.js.

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
-  
+var serveStatic = require('serve-static');
+
 var app = express();
 
 // Configuration
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' === app.get('env')) {
-    app.use(express.errorHandler());
-}
+//app.use(favicon(__dirname + '/public/favicon.ico')); - add static-favicon to dependencies, too
+//app.use(express.logger('dev')); replaced by morgan
+app.use(require('body-parser')());
+app.use(require('method-override')());
 
 // Routes
 app.get('/', routes.index);
 app.get('/authors', routes.authors);
 app.get('/books', routes.books);
+
+app.use(serveStatic(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+    app.use(require('errorhandler')());
+}
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port %d in %s mode...", app.get('port'), app.get('env'));
